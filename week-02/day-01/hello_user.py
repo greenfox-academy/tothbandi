cee0642eead038b704127999d6f9a3f23de8e10e
Toth Bandi <tot70and@gmail.com>
2017-11-27 14:38:48 +0100

    hello_user.py

--- week-02/day-01/hello_user.py
+++ week-02/day-01/hello_user.py
@@ -1,3 +1,4 @@
 # Modify this program to greet the user instead of the World!
 # The program should ask for the name of the user
-print("Hello, World!")
+name = input("Hogy hívnak?: ")
+print("Hello, " + name + "!")
