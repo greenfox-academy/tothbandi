02935c7887efe3f7156a03d30d16f357720696a6
Toth Bandi <tot70and@gmail.com>
2017-11-27 17:01:53 +0100

    draw_diagonal.py

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -10,3 +10,21 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+a = int(input("Enter a number: "))
+
+if a < 4:
+    print("Too small!")
+else:
+    for i in range(a):
+        print("%", end="")
+    print()
+    for j in range(a - 2):
+        print("%", end="")
+        for i in range(j):
+            print(" ", end="")
+        print("%", end="")
+        for i in range(a - 2 - j - 1):
+            print(" ", end="")
+        print("%")
+    for i in range(a):
+        print("%", end="")
