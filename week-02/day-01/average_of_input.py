6f13dcde820f5266467f35e158c3df1941912fe9
Toth Bandi <tot70and@gmail.com>
2017-11-27 14:55:09 +0100

    average_of_input.py

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,13 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+a = int(input("Enter the 1. integer: "))
+b = int(input("Enter the 2. integer: "))
+c = int(input("Enter the 3. integer: "))
+d = int(input("Enter the 4. integer: "))
+e = int(input("Enter the 5. integer: "))
+
+sum = a + b + c + d + e
+
+print("Sum: " + str(sum) + ", Average: " + str(sum / 5))
