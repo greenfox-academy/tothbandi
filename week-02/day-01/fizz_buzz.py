043442adf0365a32de2a2e1e0be306d3d003ff58
Toth Bandi <tot70and@gmail.com>
2017-11-27 16:10:30 +0100

    fizz_buzz.py

--- week-02/day-01/fizz_buzz.py
+++ week-02/day-01/fizz_buzz.py
@@ -1,4 +1,16 @@
 # Write a program that prints the numbers from 1 to 100.
-# But for multiples of three print “Fizz” instead of the number
-# and for the multiples of five print “Buzz”.
-# For numbers which are multiples of both three and five print “FizzBuzz”.
+# But for multiples of three print ï¿½Fizzï¿½ instead of the number
+# and for the multiples of five print ï¿½Buzzï¿½.
+# For numbers which are multiples of both three and five print ï¿½FizzBuzzï¿½.
+
+for i in range(1, 101):
+    if i % 3 == 0:
+        print("Fizz", end="")
+    if i % 5 == 0:
+        print("Buzz", end="")
+    if i % 3 == 0 or i % 5 == 0:
+        print()
+    else:
+        print(i)
+
+    
