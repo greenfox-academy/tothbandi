2dfe21123a51d99a4ae930e5338d4467e46b6ef6
Toth Bandi <tot70and@gmail.com>
2017-11-27 16:31:20 +0100

    draw_pyramid.py

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -8,3 +8,11 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+
+a = int(input("Enter a number: "))
+for i in range(1, a + 1):
+    for j in range(a - i):
+        print(" ", end="")
+    for j in range(i * 2 -1):
+        print("*", end="")
+    print()
