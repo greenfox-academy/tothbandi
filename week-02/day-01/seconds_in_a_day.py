f1fadb2615fb65b8f1844c0d86f8960f4a6aeb6d
Toth Bandi <tot70and@gmail.com>
2017-11-27 14:34:20 +0100

    seconds_in_a_day.py

--- week-02/day-01/seconds_in_a_day.py
+++ week-02/day-01/seconds_in_a_day.py
@@ -4,3 +4,5 @@ current_seconds = 42;
 
 # Write a program that prints the remaining seconds (as an integer) from a
 # day if the current time is represented bt the variables
+
+print(24*60*60 - (current_hours * 60 + current_minutes) * 60)
