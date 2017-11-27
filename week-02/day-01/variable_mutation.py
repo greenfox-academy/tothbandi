6d2e3781de90008ef48d13b5ab278258edd7f05a
Toth Bandi <tot70and@gmail.com>
2017-11-27 14:20:39 +0100

    variable_mutation.py

--- week-02/day-01/variable_mutation.py
+++ week-02/day-01/variable_mutation.py
@@ -1,6 +1,7 @@
 a = 3
 # make it bigger by 10
 
+a += 10
 
 print(a)
 
@@ -10,6 +11,7 @@ print(a)
 b = 100
 # make it smaller by 7
 
+b -= 7
 
 print(b)
 
@@ -19,6 +21,7 @@ print(b)
 c = 44
 # please double c's value
 
+c *= 2
 
 print(c)
 
@@ -28,6 +31,7 @@ print(c)
 d = 125
 # please divide by 5 d's value 
 
+d /= 5
 
 print(d)
 
@@ -37,6 +41,7 @@ print(d)
 e = 8
 # please cube of e's value
 
+e **= 3
 
 print(e)
 
@@ -47,7 +52,7 @@ f1 = 123
 f2 = 345
 # tell if f1 is bigger than f2 (pras a boolean)
 
-
+print(f1 > f2)
 
 
 
@@ -55,31 +60,32 @@ g1 = 350
 g2 = 200
 # tell if the double of g2 is bigger than g1 (pras a boolean)
 
-
+print(g2 * 2 > g1)
 
 
 h = 1357988018575474
 # tell if it has 11 as a divisor (pras a boolean)
 
 
-
+print(h % 11 == 0)
 
 
 i1 = 10
 i2 = 3
 # tell if i1 is higher than i2 squared and smaller than i2 cubed (pras a boolean)
 
-
+print(i2 ** 2 < i1 < i2 ** 3)
 
 
 j = 1521
 # tell if j is dividable by 3 or 5 (pras a boolean)
 
-
+print(j % 3 == 0 or j % 5 == 0)
 
 
 k = "Apple"
 #fill the k variable with its cotnent 4 times
 
+k *= 4
 
 print(k)
