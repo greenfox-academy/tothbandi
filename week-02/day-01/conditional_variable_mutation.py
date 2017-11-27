56a0541508a43f030a9a564fbeeea6d5ad00c107
Toth Bandi <tot70and@gmail.com>
2017-11-27 15:35:39 +0100

    conditional_variable_mutation.py

--- week-02/day-01/conditional_variable_mutation.py
+++ week-02/day-01/conditional_variable_mutation.py
@@ -2,6 +2,8 @@ a = 24
 out = 0
 # if w is even increment out by one
 
+if a % 2 == 0:
+    out += 1
 
 print(out)
 
@@ -14,6 +16,12 @@ out2 = ""
 # if less than 10 set out2 to "More!",
 # if more than 20 set out2 to "Less!"
 
+if 10 <= b and b <= 20:
+    out2 = "Sweet!"
+elif b < 10:
+    out2 = "More!"
+elif b > 20:
+    out2 = "Less!"
 
 print(out2)
 
@@ -28,6 +36,10 @@ is_bonus = False
 # and is_bonus is false decrement c by 1
 # if is_bonus is true c should remain the same
 
+if credits >= 50 and is_bonus == False:
+    c -= 2
+elif credits < 50 and is_bonus == False:
+    c -= 1
 
 print(c)
 
@@ -44,5 +56,11 @@ out3 = ""
 # set out3 to "Time out"
 # otherwise set out3 to "Run Forest Run!"
 
-
+if d % 4 == 0 and time <= 200:
+    out3 = "check"
+elif time > 200:
+    out3 = "TIme out"
+else:
+    out3 = "Run Forest Run!"
+    
 print(out3)
