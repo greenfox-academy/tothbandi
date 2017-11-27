80998aa03aa7f723ccf890c95fa00d81cfa096cf
Toth Bandi <tot70and@gmail.com>
2017-11-27 15:00:08 +0100

    odd_even.py

--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,2 +1,7 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
+number = int(input("Enter an integer: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
