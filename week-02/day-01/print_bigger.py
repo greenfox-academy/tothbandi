edd4e716d97aa35513c5fe2789188496790f11e3
Toth Bandi <tot70and@gmail.com>
2017-11-27 15:10:36 +0100

    print_bigger.py

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,9 @@
 # Write a program that asks for two numbers and prints the bigger one
+a = int(input("Enter the 1. number: "))
+b = int(input("Enter the 2. number: "))
+if a > b:
+    print("The 1. is bigger.")
+elif a < b:
+        print("The 2. is bigger.")
+else:
+    print("The numbers are equal")
