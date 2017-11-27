bd0ba2f601d9743b33d43dbb5276f0c1cb029311
Toth Bandi <tot70and@gmail.com>
2017-11-27 14:05:03 +0100

    define_basic_info.py

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,11 @@
 # Your age as an integer
 # Your height in meters as a double
 # Whether you are married or not as a boolean
+name = "Toth Bandi"
+age = 47
+height = 1.70
+married = True
+print(name)
+print(age)
+print(height)
+print(married)
