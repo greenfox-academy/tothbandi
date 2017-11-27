803cc867c5cdd878914cbf5403e1592339bd2669
Toth Bandi <tot70and@gmail.com>
2017-11-27 16:41:46 +0100

    draw_diamond.py

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,17 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+
+a = int(input("Enter a number: "))
+for i in range(1, a + 1):
+    for j in range(a - i):
+        print(" ", end="")
+    for j in range(i * 2 -1):
+        print("*", end="")
+    print()
+for i in range(1, a):
+    for j in range(i):
+        print(" ", end="")
+    for j in range((a - i) * 2 -1):
+        print("*", end="")
+    print()
