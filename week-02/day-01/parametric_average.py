4ff50a6684a472ed17e9b166966a0a383c16c3df
Toth Bandi <tot70and@gmail.com>
2017-11-27 17:38:42 +0100

    parametric_average.py

--- week-02/day-01/parametric_average.py
+++ week-02/day-01/parametric_average.py
@@ -4,3 +4,17 @@
 # integers like:
 #
 # Sum: 22, Average: 4.4
+
+print("Sum and average of numbers")
+a = 0
+sum = 0
+n = 0
+more = True
+while more:
+    a = int(input("Enter an integer: "))
+    sum += a
+    n += 1
+    more_answer = input("Is there another number? (y/n): ")
+    if more_answer == "n":
+        more = False
+print("Sum: " + str(sum) + ", Average: " + str(sum/n))
