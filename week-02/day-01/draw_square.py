9b87359d1cdc44e1ed6f8f032b8a6ffef250da00
Toth Bandi <tot70and@gmail.com>
2017-11-27 16:51:59 +0100

    draw_square.py

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,3 +10,16 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+a = int(input("Enter a number: "))
+
+for i in range(a):
+    print("%", end="")
+print()
+if a > 1:
+    for j in range(a - 2):
+        print("%", end="")
+        for i in range(a - 2):
+            print(" ", end="")
+        print("%")
+    for i in range(a):
+        print("%", end="")
