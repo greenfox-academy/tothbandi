e952e63b8116fdde4e7ac6fb4c6114ef87de8334
Toth Bandi <tot70and@gmail.com>
2017-11-27 17:47:50 +0100

    draw_chess_table.py

--- week-02/day-01/draw_chess_table.py
+++ week-02/day-01/draw_chess_table.py
@@ -9,3 +9,13 @@
 # % % % % 
 #  % % % %
 #
+for i in range(8):
+    if i % 2 == 0:
+        a = "%"
+        b = " "
+    else:
+        a = " "
+        b = "%"
+    for j in range(4):
+        print(a+b, end="")
+    print()
