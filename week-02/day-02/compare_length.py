77a28045e3cc285bc1b65015268c89195abf38e3
Toth Bandi <tot70and@gmail.com>
2017-11-28 12:22:45 +0100

    compare_length.py

---
+++ week-02/day-02/compare_length.py
@@ -0,0 +1,14 @@
+
+# - Create a variable named `p1`
+#   with the following content: `[1, 2, 3]`
+# - Create a variable named `p2`
+#   with the following content: `[4, 5]`
+# - Print if `p2` has more elements than `p1`
+
+p1 = [1, 2, 3]
+p2 = [4, 5]
+
+print(p1)
+print(p2)
+print("Second list has more elements than first?")
+print(len(p2) > len(p1))
