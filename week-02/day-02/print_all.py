7a412b352c08084961cf941270dba653c7f5798f
Toth Bandi <tot70and@gmail.com>
2017-11-28 12:15:11 +0100

    print_all.py

---
+++ week-02/day-02/print_all.py
@@ -0,0 +1,17 @@
+# - Create a variable named `af`
+#   with the following content: `[4, 5, 6, 7]`
+# - Print all the elements of `af`
+
+af = [4, 5, 6, 7]
+
+print(af)
+
+print("-----------------------")
+
+for i in af:
+    print(i)
+
+print("-----------------------")
+
+for i in range(len(af)):
+    print(af[i])
