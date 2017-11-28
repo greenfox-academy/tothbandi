1e3e23d9ad3a8eb6002706eeaf10fba0c7a778a8
Toth Bandi <tot70and@gmail.com>
2017-11-28 14:31:04 +0100

    sum_all.py

---
+++ week-02/day-02/sum_all.py
@@ -0,0 +1,16 @@
+# - Create a variable named `ai`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Print the sum of the elements in `ai`
+
+ai = [3, 4, 5, 6, 7]
+
+print(ai)
+
+sum = 0
+
+for i in ai:
+    sum += i
+
+print("The sum of the elements")
+print(sum)
+
