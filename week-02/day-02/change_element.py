0827c2445ab789d1e5c515be0a1b0df2e79b4be5
Toth Bandi <tot70and@gmail.com>
2017-11-28 12:29:09 +0100

    change_element.py

---
+++ week-02/day-02/change_element.py
@@ -0,0 +1,16 @@
+# - Create an array variable named `s`
+#   with the following content: `[1, 2, 3, 8, 5, 6]`
+# - Change the 8 to 4
+# - Print the fourth element
+
+s = [1, 2, 3, 8, 5, 6]
+
+print(s)
+
+print("Change the 8 to 4")
+
+s[3] = 4
+
+print(s
+
+)
