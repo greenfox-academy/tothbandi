b6d528c1cad026ef53e1832134102068b5bb6618
Toth Bandi <tot70and@gmail.com>
2017-11-28 12:25:46 +0100

    sum_elements.py

---
+++ week-02/day-02/sum_elements.py
@@ -0,0 +1,12 @@
+
+# - Create a variable named `r`
+#   with the following content: `[54, 23, 66, 12]`
+# - Print the sum of the second and the third element
+
+r = [54, 23, 66, 12]
+
+print(r)
+
+print("The sum of the second and the third element: ")
+
+print(r[1] + r[2])
