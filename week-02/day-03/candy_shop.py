f2f2871637c361b6fbbf5de8c07727ddb4dfc5e9
Toth Bandi <tot70and@gmail.com>
2017-11-29 12:30:54 +0100

    candy_shop.py

---
+++ week-02/day-03/candy_shop.py
@@ -0,0 +1,11 @@
+
+shop_items = ["Cupcake", 2, "Brownie", False]
+
+# Accidentally we added "2" and "false" to the list. 
+# Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+# No, don't just remove the items :)
+
+shop_items[1] = "Croissant"
+shop_items[3] = "Ice cream"
+
+print(shop_items)
