ad9873e5c662f1ac4ac3cb49c946b5d97041bead
Toth Bandi <tot70and@gmail.com>
2017-11-29 11:47:11 +0100

    todo_print

---
+++ week-02/day-03/todo_print.py
@@ -0,0 +1,21 @@
+# Add "My todo:" to the beginning of the todoText
+# Add " - Download games" to the end of the todoText
+# Add " - Diablo" to the end of the todoText but with indention
+
+# Expected outpt:
+
+# My todo:
+#  - Buy milk
+#  - Download games
+#      - Diablo
+
+todoText = "My todo:\n - Buy milk\n - Download games\n     - Diablo"
+
+print(todoText)
+
+todoText = """My todo:
+ - Buy milk
+ - Download games
+     - Diablo"""
+
+print(todoText)
