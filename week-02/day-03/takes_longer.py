7a24e2703fbe181734581bb9000a37a7a918c8ce
Toth Bandi <tot70and@gmail.com>
2017-11-29 11:43:17 +0100

    takes_longer.py

---
+++ week-02/day-03/takes_longer.py
@@ -0,0 +1,10 @@
+# When saving this quote a disk error has occured. Please fix it.
+# Add "always takes longer than" between the words "It" and "you"
+
+quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
+
+l = quote.split(" ")
+l.insert(3, "always takes longer than")
+quote = " ".join(l)
+
+print(quote)
