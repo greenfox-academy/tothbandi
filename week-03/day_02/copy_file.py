df5ae4928ee3d0417c58a84e7ab1cb3df3e0f219
Toth Bandi <tot70and@gmail.com>
2017-12-05 12:52:05 +0100

    copy_file.py

--- week-03/day_02/copy_file.py
+++ week-03/day_02/copy_file.py
@@ -1,3 +1,17 @@
 # Write a function that copies a file to an other
 # It should take the filenames as parameters
 # It should return a boolean that shows if the copy was successful
+
+def copy_file(in_file_name, out_file_name):
+    try:
+        in_file = open(in_file_name, 'r')
+        out_file = open(out_file_name, 'w')
+        out_file.write(in_file.read())
+        out_file.close()
+        in_file.close()
+        return True
+    except:
+        return False
+
+print(copy_file("my-file3.txt", "my-file4.txt"))
+
