04b613f14d591c2a60c7851b1d4984c64e6e0da7
Toth Bandi <tot70and@gmail.com>
2017-12-05 14:35:42 +0100

    reversed_lines.py

--- week-03/day_02/reversed_lines.py
+++ week-03/day_02/reversed_lines.py
@@ -1,3 +1,18 @@
 # Create a method that decrypts reversed-lines.txt
 def decrypt(file_name):
-    pass
+    try:
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        file.close()
+
+        for i in range(len(lines)):
+            lines[i] = lines[i][::-1]
+
+        file = open("backversed.txt", 'a')
+        for line in lines:
+            file.write(line)
+        file.close()
+    except FileNotFoundError:
+        print("No file")
+
+decrypt("reversed-lines.txt")
