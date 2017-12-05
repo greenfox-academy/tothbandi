be11e985eabb3915231bc9e245354622a732e9af
Toth Bandi <tot70and@gmail.com>
2017-12-05 12:34:21 +0100

    write_single_line.py

--- week-03/day_02/write_single_line.py
+++ week-03/day_02/write_single_line.py
@@ -2,3 +2,10 @@
 # Write your name in it as a single line
 # If the program is unable to write the file,
 # then it should print an error message like: "Unable to write file: my-file.txt"
+
+try:
+    file = open("my-file2.txt", "w")
+    file.write("Bandi T.")
+    file.close()
+except FileNotFoundError:
+    print("Unable to write file: my-file2.txt")
