c9fe46454304cb128109cf6e8bf64fe960cf9921
Toth Bandi <tot70and@gmail.com>
2017-12-05 15:31:20 +0100

    encoded_lines.py

--- week-03/day_02/encoded_lines.py
+++ week-03/day_02/encoded_lines.py
@@ -1,3 +1,25 @@
 # Create a method that decrypts encoded-lines.txt
 def decrypt(file_name):
-    pass
+    # try:
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        file.close()
+
+        file = open("decoded-lines.txt", 'a')
+        for line in lines:
+            a_line = ""
+            for i in range(len(line)-1):
+                if line[i] == " ":
+                    a_line += " "
+                else:
+                    a_line += chr(ord(line[i]) - 1)
+            a_line += '\n'
+            file.write(a_line)
+        file.close()
+    # except:
+        # print("there is a problem")
+
+decrypt("encoded-lines.txt")
+
+
+
