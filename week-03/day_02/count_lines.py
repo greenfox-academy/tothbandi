3f5f4dcbe8a940527e3014faf8b864b572a2095e
Toth Bandi <tot70and@gmail.com>
2017-12-05 12:27:14 +0100

    count_lines.py

--- week-03/day_02/count_lines.py
+++ week-03/day_02/count_lines.py
@@ -2,3 +2,16 @@
 # then returns the number of lines the file contains.
 # It should return zero if it can't open the file, and
 # should not raise any error.
+
+def count_lines_in(file_name):
+    try:
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        return len(lines)
+        file.close()
+
+    except FileNotFoundError:
+        return 0
+
+print(count_lines_in("my-file.txt"))
+print(count_lines_in("nincs ilyen fájl"))
