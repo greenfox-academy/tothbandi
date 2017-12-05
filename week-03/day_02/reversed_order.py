e3c1152181b4cbd9e9047a219f98663375c2371a
Toth Bandi <tot70and@gmail.com>
2017-12-05 14:48:48 +0100

    normal-order.txt

--- week-03/day_02/reversed_order.py
+++ week-03/day_02/reversed_order.py
@@ -1,3 +1,18 @@
 # Create a method that decrypts reversed-order.txt
 def decrypt(file_name):
-    pass
+    try:
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        file.close()
+
+        lines.reverse()
+
+        file = open("normal-order.txt", 'a')
+        for line in lines:
+            file.write(line)
+        file.close()
+    except:
+        print("any problem")
+
+decrypt("reversed-order.txt")
+
