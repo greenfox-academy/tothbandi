102af2c50ec822b05cd50f29b202ccfc344857da
Toth Bandi <tot70and@gmail.com>
2017-12-05 14:18:39 +0100

    doubled.py

--- week-03/day_02/doubled.py
+++ week-03/day_02/doubled.py
@@ -1,4 +1,28 @@
 # Create a method that decrypts the duplicated-chars.txt
 
 def decrypt(file_name):
-    pass
+    try:
+        file = open(file_name, 'r')
+        lines = file.readlines()
+        file.close()
+
+        for j in range(len(lines)):
+            new_line = ""
+            for i in range(len(lines[j])):
+                if i % 2 == 0:
+                    new_line += lines[j][i]
+            lines[j] = new_line
+
+        file = open("simplicated.txt", 'a')
+        for j in range(len(lines)):
+            file.write(lines[j])
+        file.close()
+    except FileNotFoundError:
+        print("No file")
+
+
+decrypt("duplicated-chars.txt")
+
+
+
+        
