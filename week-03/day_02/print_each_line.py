4ba58958942f46da9ba7b17f578d4707b15ae441
Toth Bandi <tot70and@gmail.com>
2017-12-05 12:05:47 +0100

    print_each_line.py

--- week-03/day_02/print_each_line.py
+++ week-03/day_02/print_each_line.py
@@ -2,3 +2,33 @@
 # each of lines form the file.
 # If the program is unable to read the file (for example it does not exists),
 # then it should print an error message like: "Unable to read file: my-file.txt"
+
+
+def prints_file(file_name, ver = 0):
+    try:
+        file = open(file_name, 'r')
+        if ver == 0:
+            for line in file:
+                print(line, end='')
+            print()
+        elif ver == 1:
+            lines = file.readlines()
+            for line in lines:
+                print(line, end='')
+            print()
+        else:
+            print("no such parameter.")
+        file.close()
+
+    except FileNotFoundError:
+        print("Unable to read file: my-file.txt")
+
+prints_file("my-file.txt")
+prints_file("my-file.txt", 1)
+prints_file("my-file.txt", 2)
+print("print - BEE: ")
+prints_file("BEE")
+
+
+
+
