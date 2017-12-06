557f76958ce6a79f8a60b20b59d92de10f3d6450
Toth Bandi <tot70and@gmail.com>
2017-12-06 10:28:31 +0100

    line_in_the_middle.py

---
+++ week-03/day-03/line_in_the_middle.py
@@ -0,0 +1,17 @@
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300', bg = 'white')
+canvas.pack()
+
+# draw a red horizontal line to the canvas' middle.
+
+h_line = canvas.create_line(0, 150, 300, 150, fill = 'red')
+
+# draw a green vertical line to the canvas' middle.
+
+v_line = canvas.create_line(150, 0, 150, 300, fill = 'green')
+
+root.mainloop()
