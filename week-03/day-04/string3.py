0076b6072709362535e0c9a925528d3cf84958d5
Toth Bandi <tot70and@gmail.com>
2017-12-07 12:54:25 +0100

    string3.py

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,13 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+string = "This text is with stars separated"
+
+def str_corrector(string):
+    c = string[0]
+    if len(string) == 1:
+        return c
+    else:
+        return c + '*' + str_corrector(string[1:])
+
+print(str_corrector(string))
