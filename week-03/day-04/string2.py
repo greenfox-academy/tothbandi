3c4fda3e99c74fcff30c9ae55c7511409fb19f5f
Toth Bandi <tot70and@gmail.com>
2017-12-07 12:47:29 +0100

    string2.py

--- week-03/day-04/string2.py
+++ week-03/day-04/string2.py
@@ -1,1 +1,14 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+string = "This text is still misstyped, but no x in it, and recursion is good"
+
+def str_corrector(string):
+    c = string[0]
+    if string[0] == 'x':
+        c = ''
+    if len(string) == 1:
+        return c
+    else:
+        return c + str_corrector(string[1:])
+
+print(str_corrector(string))
