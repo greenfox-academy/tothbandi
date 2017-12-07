64a96158e8129ac232f2e08089ae01695ed1d29a
Toth Bandi <tot70and@gmail.com>
2017-12-07 12:23:52 +0100

    bunny2.py

--- week-03/day-04/bunny2.py
+++ week-03/day-04/bunny2.py
@@ -2,3 +2,23 @@
 # (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 # have 3 ears, because they each have a raised foot. Recursively return the
 # number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+def sum_bunny_ears(n):
+    ear = 0
+
+    if n % 2 == 0:
+        ear = 3
+    else:
+        ear = 2
+
+    if n==1:
+        return 2
+    else:
+        return ear + sum_bunny_ears(n-1)
+
+print(sum_bunny_ears(1))
+print(sum_bunny_ears(2))
+print(sum_bunny_ears(3))
+print(sum_bunny_ears(4))
+print(sum_bunny_ears(5))
+
