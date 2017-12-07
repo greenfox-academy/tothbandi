b956875bca01b5988ca9ce272d2bde078cc09b46
Toth Bandi <tot70and@gmail.com>
2017-12-07 11:43:48 +0100

    number_adder.py

--- week-03/day-04/number_adder.py
+++ week-03/day-04/number_adder.py
@@ -1,1 +1,12 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+n = int(input("enter a positiv integer :"))
+
+def summa(n):
+    if n == 1:
+        return 1
+    else:
+        return n + summa(n-1)
+
+print(summa(n))
+
