8bb5f4d90d5c85684429ff3386c0fbd04fc65bbd
Toth Bandi <tot70and@gmail.com>
2017-12-07 12:43:44 +0100

    string1.py

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,2 +1,15 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+string = "This teyt is still misstyped, but recursion is good"
+
+def str_corrector(string):
+    c = string[0]
+    if string[0] == 'y':
+        c = 'x'
+    if len(string) == 1:
+        return c
+    else:
+        return c + str_corrector(string[1:])
+
+print(str_corrector(string))
