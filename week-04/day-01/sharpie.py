2bb4225f4f09756ed303b09c86123b2e643b2434
toth bandi <tot70and@gmail.com>
2017-12-11 16:27:07 +0100

    sharpie.py

---
+++ week-04/day-01/sharpie.py
@@ -0,0 +1,22 @@
+from random import random
+
+class Sharpie(object):
+    def __init__(self, color, width, ink_amount = 100.0):
+        self.color = color
+        self.width = width
+        self.ink_amount = ink_amount
+
+    def use(self):
+        self.ink_amount *= random()
+    
+pen = Sharpie("green", 3.5)
+
+print("The pen color is {}, width is {} and the amount of the ink is {}.".format(
+    pen.color, pen.width, pen.ink_amount
+))
+
+pen.use()
+
+print("The pen color is {}, width is {} and the amount of the ink is {}.".format(
+    pen.color, pen.width, pen.ink_amount
+))
