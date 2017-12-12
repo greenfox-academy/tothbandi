4dc6616eb05d90ecf5ef62e03185d340f9ad5f1e
toth bandi <tot70and@gmail.com>
2017-12-12 12:50:33 +0100

    Garden ex - plant.py

---
+++ week-04/day-02/plant.py
@@ -0,0 +1,11 @@
+class Plant(object):
+    def __init__(self, color, water_amount = 0):
+        self.color = color
+        self.water_amount = water_amount
+    
+    def watering(self, amount_of_water):
+        self.water_amount += amount_of_water
+
+    def needs_water(self):
+        return self.water_amount < 20
+    
