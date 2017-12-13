65a55cf20fdf0e7c4a961bffff006adb3698d4f4
toth bandi <tot70and@gmail.com>
2017-12-13 12:26:16 +0100

    sum

--- week-04/day-03/toth_bandi_test.py
+++ week-04/day-03/toth_bandi_test.py
@@ -3,8 +3,29 @@ from toth_bandi_work import MyClass
 
 class TestTothBandi(unittest.TestCase):
     def test_apple(self):
-        apple = Apple()
-        self.assertEquals(apple.get_apple(), 'quince')
+        apple = MyClass()
+        self.assertEqual(apple.get_apple(), 'quince')
+
+    def test_sum_of_list_of_numbers(self):
+        sum_of_list = MyClass()
+        self.assertEqual(sum_of_list.summa([3, 4, 5]), 12)
+
+    def test_sum_of_empty_list(self):
+        sum_of_list = MyClass()
+        self.assertEqual(sum_of_list.summa([]), 0)
+
+    def test_sum_of_one_element_list(self):
+        sum_of_list = MyClass()
+        self.assertEqual(sum_of_list.summa([5]), 5)    
+
+    def test_sum_of_null(self):
+        sum_of_list = MyClass()
+        self.assertEqual(sum_of_list.summa(), 0)
+    
+    def test_anagram(self):
+        anagram = MyClass()
+        self.assertEqual(anagram.is_anagram('orrszarvu', 'rovar ruzs'), True)
+
 
 if __name__ == '__main__':
     unittest.main()
--- week-04/day-03/toth_bandi_work.py
+++ week-04/day-03/toth_bandi_work.py
@@ -1,3 +1,12 @@
 class MyClass():
     def get_apple(self):
         return 'quince'
+
+    def summa(self, numbers = []):
+        summa = 0
+        for number in numbers:
+            summa += number
+        return summa
+
+    def is_anagram(self, string1, string2):
+        return True
