9c3581fd709ccbd103ed9766b437e8c7e6e8bae3
tothbandi <tot70and@gmail.com>
2017-12-19 12:27:13 +0100

    feat(view) add get_board_width, get_board_height

--- week-05/rpg/view.py
+++ week-05/rpg/view.py
@@ -4,7 +4,7 @@ import tile
 
 class View(object):
     def __init__(self, board):
-        self.board = board
+        self.board = board.get_board()
         self.root = Tk()
     
     def get_tile_width(self):
@@ -19,13 +19,18 @@ class View(object):
     
     def get_board_width(self):
         tile_width = self.get_tile_width()
+        tiles_in_row = len(self.board[0])
+        return tiles_in_row * tile_width
+
+    def get_board_height(self):
         tile_height = self.get_tile_height()
-
-
-        self.board.get_board
+        tiles_in_col = len(self.board)
+        return tiles_in_col * tile_height
 
     def set_canvas_size(self):
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight() - 168 # because of Windows taskbar
         board_width = self.get_board_width()
+        board_height = self.get_board_height()
         print('set_canvas_size')
+        print(str(board_width) + ' ' + str(board_height))
