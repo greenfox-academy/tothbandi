1b454e93a73e776075be6bd4657a0af121bc102a
tothbandi <tot70and@gmail.com>
2017-12-19 11:19:04 +0100

    feat(main) resize the tiles and canvas to the size of screen

--- week-05/rpg/main.py
+++ week-05/rpg/main.py
@@ -1,9 +1,34 @@
 from tkinter import *
+from resizeimage import resizeimage
 import tile
 import maps
 
 root = Tk()
-canvas = Canvas(root, width = 724, height = 724)
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight() - 168 # because of Windows taskbar
+atile = tile.Floor(0,0)
+img = PhotoImage(file = atile.image)
+
+board_width = 10 * img.width()
+board_height = 10 * img.height()
+
+width_ratio = screen_width / board_width
+if width_ratio > 1.0:
+    width_ratio = 1.0
+height_ratio = screen_height / board_height
+if width_ratio > 1.0:
+    width_ratio = 1.0
+ratio = width_ratio if width_ratio < height_ratio else height_ratio
+canvas_width = board_width * ratio + 4
+canvas_height = board_height * ratio + 4
+
+print(ratio)
+
+print(str(board_width * ratio) + ' ' + str(board_height *ratio))
+
+canvas = Canvas(root, width = canvas_width, height = canvas_height)
+print(canvas)
 canvas.pack()
 a_map = maps.Maps()
 area = a_map.get_map()
@@ -16,14 +41,13 @@ for i in range(10):
     images.append([])
     for j in range(10):
         if area[i][j] == '0':
-            print('0')
             tiles[i].append(tile.Floor(4 + j * 72, 4 + i * 72))
             images[i].append(PhotoImage(file = tiles[i][j].image))
-            canvas.create_image(tiles[i][j].posx, tiles[i][j].posy, anchor = 'nw', image = images[i][j])
+            iid = canvas.create_image(tiles[i][j].posx, tiles[i][j].posy, anchor = 'nw', image = images[i][j])
+            canvas.scale(iid, 0, 0, ratio, ratio)
         else:
             tiles[i].append(tile.Wall(4 + j * 72, 4 + i * 72))
-            print(tiles[i][j].image)
             images[i].append(PhotoImage(file = tiles[i][j].image))
-            canvas.create_image(tiles[i][j].posx, tiles[i][j].posy, anchor = 'nw', image = images[i][j])
-            
+            iid = canvas.create_image(tiles[i][j].posx, tiles[i][j].posy, anchor = 'nw', image = images[i][j])
+            canvas.scale(iid, 0, 0, ratio, ratio)
 root.mainloop()
