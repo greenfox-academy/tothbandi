b3c19af4097b183a424c13a722c5adf58291dcc0
tothbandi <tot70and@gmail.com>
2017-12-19 17:21:40 +0100

    refactor(boards) move get_row, get_col from view

--- week-05/rpg/boards.py
+++ week-05/rpg/boards.py
@@ -3,16 +3,22 @@ class Boards(object):
     def __init__(self):
         self.level = 1
         self.input = 'level{}.txt'
+        self.board = []
+        self.get_board()
 
     def get_file_name(self):
         return self.input.format(str(self.level).zfill(3))
 
     def get_board(self):
-        board = []
         try:
             with open(self.get_file_name()) as file:
-                board = file.read().split('\n')
-            return board
+                self.board = file.read().split('\n')
+            # return board
         except FileNotFound:
             return ''
-        
+
+    def get_row(self): # boardba
+        return len(self.board[0])
+    
+    def get_col(self): # boardba
+        return len(self.board)    
