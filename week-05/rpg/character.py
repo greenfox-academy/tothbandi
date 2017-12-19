2c2f23728fcdf78242fbb6bc08e57a6fda804133
tothbandi <tot70and@gmail.com>
2017-12-19 15:20:06 +0100

    feat(character) add Charracter class, Hero class

---
+++ week-05/rpg/character.py
@@ -0,0 +1,13 @@
+class Character(object):
+    def __init__(self, max_health_point, current_health_point, defend_point, strike_point):
+        self.max_health_point = max_health_point
+        self.current_health_point = current_health_point
+        self.defend_point = defend_point
+        self.strike_point = strike_point
+
+class Hero(Character):
+    image = 'hero.png'
+    def __init__(self, max_health_point, current_health_point, defend_point, strike_point):
+        super().__init__(max_health_point, current_health_point, defend_point, strike_point)
+
+    
