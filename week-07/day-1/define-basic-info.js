8b4a0dae78068104a3f96c09dcbd6bee580c1d28
tothbandi <tot70and@gmail.com>
2018-01-08 12:28:54 +0100

    feat define-basic-info.js

---
+++ week-07/day-1/define-basic-info.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Bandi';
+let age = 47;
+let height = 1.70;
+let married = true;
+
+console.log('Name: ' + name);
+console.log('Age: ' + age);
+console.log('Height: ' + height);
+console.log('Married: ' + married);
