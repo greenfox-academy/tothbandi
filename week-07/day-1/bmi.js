79d9002fed2b55add6f74ebd6337bd9f57205ded
tothbandi <tot70and@gmail.com>
2018-01-08 12:24:03 +0100

    feat bmi.js

---
+++ week-07/day-1/bmi.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+var massInKg = 81.2;
+var heightInM = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+var bmi = 0;
+
+bmi = massInKg / (heightInM * heightInM);
+
+console.log('Mass is: ' + massInKg + 'kg.');
+console.log('Height id: ' + heightInM + 'm.');
+console.log('BMI is: ' + bmi + '.');
