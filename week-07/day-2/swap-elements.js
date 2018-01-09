f67d2b1fa213c50627101dcea87764577f83cdc8
tothbandi <tot70and@gmail.com>
2018-01-09 12:58:08 +0100

    feat swap-elements.js

---
+++ week-07/day-2/swap-elements.js
@@ -0,0 +1,14 @@
+
+'use strict';
+// - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// - Swap the first and the third element of `abc`
+
+let abc = ["Arthur", "Boe", "Chloe"];
+
+console.log(abc);
+
+let element = abc[0];
+abc[0] = abc[2];
+abc[2] = element;
+
+console.log(abc);
