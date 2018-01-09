1fef7043043530f281649fba6024c6732286ef4a
tothbandi <tot70and@gmail.com>
2018-01-09 17:51:29 +0100

    feat element-finder.js

---
+++ week-07/day-2/element-finder.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Check if the array contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+var numbers = [1,2,3,4,5,6,8];
+
+console.log(numbers.indexOf(7) === -1 ? 'Nooooooo' : 'Hooooray');
