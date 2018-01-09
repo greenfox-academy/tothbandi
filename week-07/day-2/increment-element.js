3a433b700e4d112851a52fd3b228907011033276
tothbandi <tot70and@gmail.com>
2018-01-09 14:04:03 +0100

    feat increment-element.js

---
+++ week-07/day-2/increment-element.js
@@ -0,0 +1,8 @@
+'use strict';
+// - Create a variable named `t` with the following content: `[1, 2, 3, 4, 5]`
+// - Increment the third element simply by accessing it
+// - Log the third element to the console
+
+let t = [1, 2, 3, 4, 5];
+
+console.log(++t[2]);
