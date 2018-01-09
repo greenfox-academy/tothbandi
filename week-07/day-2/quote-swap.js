dba8b0449b475b83d6c72aa2b4ba3242d75f012b
tothbandi <tot70and@gmail.com>
2018-01-09 19:06:23 +0100

    feat quote-swap.js

---
+++ week-07/day-2/quote-swap.js
@@ -0,0 +1,12 @@
+'use strict';
+// Accidentally I messed up this quote from Richard Feynman.
+// Two words are out of place
+// Your task is to fix it by swapping the right words with code
+
+// Also, log the sentence to the console with spaces in between.
+
+var words = ["What", "I", "do", "create,", "I", "cannot", "not", "understand."];
+
+[words[2], words[5]] = [words[5], words[2]];
+
+console.log(words.join(' '));
