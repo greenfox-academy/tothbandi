f93d3885ddd02b49b6d8d453e66c5de1a4f2e9f1
tothbandi <tot70and@gmail.com>
2018-01-09 14:44:37 +0100

    feat sum-all.js

---
+++ week-07/day-2/sum-all.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// - Log the sum of the elements in `ai` to the console
+
+let ai = [3, 4, 5, 6, 7];
+
+let sum = ai.reduce(function(a, b){
+  return a + b;
+});
+
+console.log(sum);
