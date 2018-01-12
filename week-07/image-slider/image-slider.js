499628a6ff08791361429ddf11a979080d8ae0fc
tothbandi <tot70and@gmail.com>
2018-01-12 12:16:31 +0100

    feat image-slider control left, right arrow keys

--- week-07/image-slider/image-slider.js
+++ week-07/image-slider/image-slider.js
@@ -46,15 +46,28 @@ const leftBtn = document.querySelector('.left');
 const photoTitle = document.querySelector('.title');
 const photoDesc = document.querySelector('.story');
 let activeThumb = 0;
-// console.log(document.querySelectorAll('li'));
-// consol.log(thumbBg);
-// let a = document.querySelector('.chosen-photo');
-// a.style.backgroundImage = 'url("images/sample.jpg")';
-
-// let lis = document.querySelectorAll('.thumbnail');
-// lis[0].style.backgroundImage
 
 rightBtn.addEventListener('click', (event) => {
+  rightEvent();
+});
+
+leftBtn.addEventListener('click', (event) => {
+  leftEvent();
+});
+
+window.addEventListener('keyup', keyPressed);
+
+function keyPressed(event){
+  if(event.code === 'ArrowRight'){
+    rightEvent();
+  }
+  if(event.code === 'ArrowLeft'){
+    leftEvent();
+  }
+  // document.querySelector('h1').innerText = 'Last pressed key code is: ' + event.code;
+}
+
+function rightEvent(){
   if (activeThumb < thumbnails.length - 1){
     setNormalThumbnail();
     activeThumb++;
@@ -62,9 +75,9 @@ rightBtn.addEventListener('click', (event) => {
   } else {
     oneStepLeft();
   }
-});
+}
 
-leftBtn.addEventListener('click', (event) => {
+function leftEvent(){
   if (activeThumb > 0){
     setNormalThumbnail();
     activeThumb--;
@@ -72,12 +85,12 @@ leftBtn.addEventListener('click', (event) => {
   } else {
     oneStepRight();
   }
-});
+}
+
 
 function oneStepLeft(){
   for(let i = 1; i < thumbnails.length; i++){
     thumbnails[i-1].style.backgroundImage = getFileFromThumb(i);
-    // thumbnails[i-1].setAttribute('data-index', thumbnails[i].dataset.index);
     thumbnails[i-1].setAttribute('data-photosindex', thumbnails[i].dataset.photosindex);
   }
   let lastPhotosIndex = thumbnails[thumbnails.length - 1].dataset.photosindex;
@@ -87,8 +100,6 @@ function oneStepLeft(){
   } else {
     lastPhotosIndex = 0;
   }
-  console.log(lastPhotosIndex);
-  console.log(photos[lastPhotosIndex].file);
   thumbnails[thumbnails.length - 1].style.backgroundImage = 'url("images/' + photos[lastPhotosIndex].file + '")'; 
   thumbnails[thumbnails.length - 1].setAttribute('data-photosindex', lastPhotosIndex);
   setChosenPhoto(thumbnails[activeThumb].dataset.photosindex);    
@@ -97,7 +108,6 @@ function oneStepLeft(){
 function oneStepRight(){
   for(let i = thumbnails.length - 2; i >= 0 ; i--){
     thumbnails[i+1].style.backgroundImage = getFileFromThumb(i);
-    // thumbnails[i-1].setAttribute('data-index', thumbnails[i].dataset.index);
     thumbnails[i+1].setAttribute('data-photosindex', thumbnails[i].dataset.photosindex);
   }
   let firstPhotosIndex = thumbnails[0].dataset.photosindex;
@@ -106,8 +116,6 @@ function oneStepRight(){
   } else {
     firstPhotosIndex = photos.length - 1;
   }
-  console.log(firstPhotosIndex);
-  console.log(photos[firstPhotosIndex].file);
   thumbnails[0].style.backgroundImage = 'url("images/' + photos[firstPhotosIndex].file + '")'; 
   thumbnails[0].setAttribute('data-photosindex', firstPhotosIndex);
   setChosenPhoto(thumbnails[activeThumb].dataset.photosindex);  
@@ -121,32 +129,20 @@ function setNormalThumbnail() {
 
 thumbsCont.addEventListener('click', function(event){
   setNormalThumbnail();
-
-  // console.log(event.target.dataset.index);  //getAttribute('style'));
   activeThumb = event.target.dataset.index;
   setChosenThumbnail(activeThumb);
 
 });
 
 function setChosenPhoto(photoIndex){
-  // let str = 'url("images/' + photo.file + '")';
-
-  // console.log(str);
   chosenPhoto.style.backgroundImage = 'url("images/' + photos[photoIndex].file + '")';
   photoTitle.textContent = photos[photoIndex].title;
   photoDesc.textContent = photos[photoIndex].story;
 
 }
 
-// setChosenPhoto(photos[1]);
-
-// thumbnails.forEach((thumbnail, index, array) => {
-//   thumbnail.style.backgroundImage = 'url("images/' + photos[index].file + '")';
-// });
-
 function styleChosenThumbnail(index){
   thumbBg[index].style.background='linear-gradient(to bottom, grey, white)' ; //setAttribute('background-color', 'red')); // .getAttribute('width'));
-  //   box-shadow: 0 10px 5px gray;
   thumbBg[index].style.boxShadow = '0 -10px 5px grey';
 }
 
@@ -157,10 +153,6 @@ function getFileFromThumb(index){
 }
 
 function setChosenThumbnail(index){
-  // let file = thumbnails[index].getAttribute('style');
-  // file = file.substring(18, file.length-1);
-  // console.log(file);
-// getFileFromThumb(index)
   setChosenPhoto(thumbnails[index].dataset.photosindex);
   styleChosenThumbnail(index);
 }
