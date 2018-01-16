82dc9efd1183f74b38f5fa2ea3dee669d97a1615
tothbandi <tot70and@gmail.com>
2018-01-16 12:05:55 +0100

    feat multiplier.js

---
+++ week-08/day-2/multiplier.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let multiplier = function(factor){
+  function multiply(number){
+    return number * factor;
+  }
+  return multiply;
+}
+
+
+// Create the multiplier function that you can use like this:
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
