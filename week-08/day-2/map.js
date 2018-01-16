2d2b6e4019044d00a90250608da54c4a1e67ffb6
tothbandi <tot70and@gmail.com>
2018-01-16 15:17:06 +0100

    feat map.js

---
+++ week-08/day-2/map.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var fruits = [
+  'melon',
+  'apple',
+  'strawberry',
+  'blueberry',
+  'pear',
+  'banana'
+];
+
+// Create a new array of consists numbers that shows how many times the 'e' letter
+// occurs in the word stored under the same index at the fruits array!
+// Please use the map method.
+
+let eLetter = fruits.map(function(fruit) {
+  return fruit.split('e').length - 1;
+});
+
+console.log(eLetter);
