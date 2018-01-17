6091747596e4d18c730c587ddcf930c93acb347b
tothbandi <tot70and@gmail.com>
2018-01-17 15:55:08 +0100

    feat New York Times API

---
+++ week-08/day-3/nyt/nytquery.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?api-key=6bb5e94b1dd24f45a8962bb40df2ac1e&q=the moon landing by Apollo 11';
+
+let myRequest = new XMLHttpRequest();
+myRequest.open('GET', url, true);
+myRequest.send(null);
+myRequest.onload = function(){
+  let myData = JSON.parse(myRequest.responseText);
+
+  let body = document.querySelector('body');
+
+  let ul = document.createElement('ul');
+
+  for(let i = 0; i < 10; i++){
+    let article = myData.response.docs[i];
+    let li = document.createElement('li');
+    let headline = document.createElement('h3');
+    let snippet = document.createElement('p');
+    let date = document.createElement('p');
+    headline.textContent = article.headline.main;
+    snippet.textContent = article.snippet;
+    date.textContent = article.pub_date.substr(0, 10);
+    li.appendChild(headline);
+    li.appendChild(snippet);
+    li.appendChild(date);
+    ul.appendChild(li);
+  }
+
+  body.appendChild(ul);
+
+  // console.log(myData);
+
+
+};
+
+// console.log(window.location.protocol + "//" + window.location.host + "/" + window.location.pathname)
+
+// myData.response.docs[index]
+// // .headline.main
+// // .snippet
+// // .pub_date first ten char
