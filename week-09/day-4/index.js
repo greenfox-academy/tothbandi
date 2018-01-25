ef61ac949418ebbda09acc95cb211e2102dc43a9
tothbandi <tot70and@gmail.com>
2018-01-25 16:12:57 +0100

    feat doubling

--- week-09/day-4/index.js
+++ week-09/day-4/index.js
@@ -1,14 +1,27 @@
 'use strict';
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8081);
+app.get('/doubling', (req, res) => {
+    console.log('====', req.query.input);
+    let resObj = {};
+    if(req.query.input === undefined){
+        resObj.error = "Please provide an input!";
+    } else {
+        resObj.received = req.query.input;
+        resObj.result = req.query.input * 2;
+    }
+    res.send(res.json(resObj));
+});
+
+app.listen(8080);
 
