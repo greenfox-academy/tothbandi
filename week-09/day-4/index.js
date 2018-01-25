4f169bd3adbdf7660ef416d11d30f8fe1afaf847
tothbandi <tot70and@gmail.com>
2018-01-25 12:50:44 +0100

    feat frontend

---
+++ week-09/day-4/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8081);
+
