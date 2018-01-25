cabc9ace64dd3af82db6e3ebc02b8376eb7947a7
tothbandi <tot70and@gmail.com>
2018-01-25 17:29:38 +0100

    feat do until

--- week-09/day-4/index.js
+++ week-09/day-4/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
@@ -42,6 +44,20 @@ app.get('/appenda/:appendable', (req, res) => {
 app.get('/appenda', (req, res) => {
     res.status(404);
     res.send();
+});
+
+app.post('/dountil/:action', (req, res) => {
+    let resObj = {};
+    let n = req.body.until;
+    if (req.params.action === 'sum') {
+        resObj.result = n * (n + 1) / 2;
+    } else if (req.params.action === 'factor') {
+        resObj.result = 1;
+        for(let i = 1; i <= n; i++) {
+            resObj.result *= i;
+        }
+    }
+    res.send(res.json(resObj));
 });
 
 app.listen(8080);
